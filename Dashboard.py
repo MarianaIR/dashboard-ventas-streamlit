d2c11534a5763a1fea11343931f1c8e925702cfa
Mariana Ibarra <105397276+MarianaIR@users.noreply.github.com>
2025-11-26 17:35:54 -0300

    Dejando los meses en ingles

--- Dashboard.py
+++ Dashboard.py
@@ -76,7 +76,7 @@ fact_ciudades = datos.drop_duplicates(subset="Lugar de Compra")[["Lugar de Compr
 facturacion_mensual = datos.set_index("Fecha de Compra").groupby(pd.Grouper(freq="ME"))["Precio"].sum().reset_index()
 
 facturacion_mensual["Año"] = facturacion_mensual["Fecha de Compra"].dt.year
-facturacion_mensual["Mes"] = facturacion_mensual["Fecha de Compra"].dt.month_name("es")
+facturacion_mensual["Mes"] = facturacion_mensual["Fecha de Compra"].dt.month_name()
 
 facturacion_cat = datos.groupby("Categoría del Producto")[["Precio"]].sum().sort_values("Precio", ascending=False)
 
